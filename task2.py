c3b0c142e662673429d7a4f3c53715d3576d1da6
Bezil <44468031+BartekKarwacki@users.noreply.github.com>
2020-09-02 00:34:17 +0200

    2 ended tasks

--- task2.py
+++ task2.py
@@ -7,39 +7,57 @@ zmiennoprzecinkowymi, to należałoby, to zrobić tak:
 min_value = random.random() * 150
 max_value = random.random() * 125 + 75
 oraz input_value typować na float.
+
+Została złamana zasada 80 znaków w linijce
+przez specyfikę printa.
 """
 
 import random
 
 
 def get_values():
+    """
+        Returns  2 random values.
+        min_value is always in range 0 - 150,
+        max_value is always range 75 - 200 and
+        greater than min_value.
+    """
     min_value = max_value = 0
     while max_value <= min_value:
         min_value = random.randint(0, 150)
         max_value = random.randint(75, 200)
     return min_value, max_value
 
-def input_value(min_value, max_value):
+def input_number(message):
+    """
+        Returns user input if its valid.
+    """
     while True:
         try:
-            input_value = int(input(f'Input number in range {min_value} - {max_value}: '))
+            input_value = int(input(message))
             return input_value
         except ValueError:
             print('Input must be a number!')
             continue
 
-def is_number_in_range(min_value, max_value):
+def is_number_in_range(min_value, max_value, message):
+    """
+        Checks if user input is between
+        closed group of min_value and max_value.
+        If true then ends function.
+    """
     while True:
-        value = input_value(min_value, max_value)
+        value = input_number(message)
         if min_value <= value <= max_value:
             print(f'Input {value} is in range.')
             return
         elif min_value > value:
             print("The value you've entered is smaller than the minimum value allowed.")
         else:
-            print("The value you've entered is greater than the maximum value allowed")
+            print("The value you've entered is greater than the maximum value allowed.")
 
 
 if __name__ == "__main__":
     min_value, max_value = get_values()
-    is_number_in_range(min_value, max_value)
+    message = f'Input number in range {min_value} - {max_value}: '
+    is_number_in_range(min_value, max_value, message)
